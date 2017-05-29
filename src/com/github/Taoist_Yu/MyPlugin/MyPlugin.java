e51f32f9de039dde25906093ac8e8d4d62a4ed6a
Taoist-Yu <294196463@qq.com>
2017-05-29 11:49:22 +0800

    提交项目

---
+++ src/com/github/Taoist_Yu/MyPlugin/MyPlugin.java
@@ -0,0 +1,17 @@
+package com.github.Taoist_Yu.MyPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class MyPlugin extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		getLogger().info("插件加载成功！");
+	}
+	
+	@Override
+	public void onDisable() {
+		getLogger().info("插件成功退出！");
+	}
+	
+}
