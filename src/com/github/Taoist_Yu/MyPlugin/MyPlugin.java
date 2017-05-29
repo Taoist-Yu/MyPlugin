0e5b2ca20d597347f359427c673deae07678f570
Taoist-Yu <294196463@qq.com>
2017-05-29 17:32:54 +0800

    添加了控制台指令：fireBall

--- src/com/github/Taoist_Yu/MyPlugin/MyPlugin.java
+++ src/com/github/Taoist_Yu/MyPlugin/MyPlugin.java
@@ -1,8 +1,10 @@
 package com.github.Taoist_Yu.MyPlugin;
 
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
 
 public final class MyPlugin extends JavaPlugin {
 
@@ -21,6 +23,20 @@ public final class MyPlugin extends JavaPlugin {
 		if(cmd.getName().equalsIgnoreCase("testCommand")) {
 			getLogger().info("指令被触发");
 			return true;
+		} else if(cmd.getName().equalsIgnoreCase("fireBall")) {
+			if(args.length != 1){
+				return false;
+			} 
+			
+			Player target = Bukkit.getServer().getPlayer(args[0]);
+			
+			if(target == null) {
+				Sender.sendMessage("无法找到" + args[0]);
+				return false;
+			}
+			
+			target.setFireTicks(100);
+			
 		}
 		
 		return false;
